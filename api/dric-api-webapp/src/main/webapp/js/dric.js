48ec1c236c52e8d79305789b55e19d0b80783944
James Caple <jcaple>
2015-06-19 19:55:39 -0400

    Advanced Search UI start

---
+++ api/dric-api-webapp/src/main/webapp/js/dric.js
@@ -0,0 +1,12 @@
+/**
+ * Javascript routines supporting DRIC application.
+ * @author: jcaple, 6/20/2015
+ */
+
+var dric = {
+
+	revealAdvancedSearch : function() {
+		$("#advancedFilters").show();	
+	}
+
+};
