b801ea7501e45f924b4ab873aa625832d19428ee
larry.cutlip-mason@devtechnology.com <larry.cutlip-mason@devtechnology.com>
2015-06-19 16:32:55 -0400

    generalize the path to just the local

--- Docker/ControlScripts/build-dric-container.sh
+++ Docker/ControlScripts/build-dric-container.sh
@@ -4,5 +4,5 @@
 curl -O  https://raw.githubusercontent.com/DevTechnology/DRIC/master/Docker/dockerfile
 
 #Execute the build
-sudo docker build -f /home/centos/dric/container/dockerfile --tag=dtg/dric .
+sudo docker build -f dockerfile --tag=dtg/dric .
 
