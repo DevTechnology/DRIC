b4679d6d7b86517a569ff935a33b1d9e905d3c2a
lcutlip <lcutlip@devtechnology.com>
2015-06-29 10:50:02 -0400

    Change to Control Scripts - Strip Sudo out, exepectation is script itself is run with Sudo

--- Docker/ControlScripts/update-control-scripts.sh
+++ Docker/ControlScripts/update-control-scripts.sh
@@ -3,4 +3,4 @@
 curl -O  https://raw.githubusercontent.com/DevTechnology/DRIC/master/Docker/ControlScripts/build-dric-container.sh
 curl -O  https://raw.githubusercontent.com/DevTechnology/DRIC/master/Docker/ControlScripts/start-jboss.sh
 curl -O  https://raw.githubusercontent.com/DevTechnology/DRIC/master/Docker/ControlScripts/stop-jboss.sh
-sudo chmod 755 *.sh
+chmod 755 *.sh
