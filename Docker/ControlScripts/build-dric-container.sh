eb050a5e3efd4c26a63d7d50e9d31de2a8805ada
lawrence.cutlip-mason <larry.cutlip-mason>
2015-06-30 13:57:07 -0400

    working prod build with download of the release war

--- Docker/ControlScripts/build-dric-container.sh
+++ Docker/ControlScripts/build-dric-container.sh
@@ -1,5 +1,9 @@
  #!/bin/bash
  
+# These 2 properties specify the version to build, change if wanted/necessary
+file="dric-api-webapp-1.0-SNAPSHOT.war"
+version="v1.0"
+
 # You can specify the type of file you want to use, test/debug or prod. Valid parms are "debug", "prod" or no parameter.
 # Example: sudo ./build-dric-container.sh debug
 if [ -z "$1" ]
@@ -16,8 +20,23 @@ if [ -z "$1" ]
   	exit
 fi
  
-#Download from GIT
+#Download dockerfile from GIT
 curl -O  https://raw.githubusercontent.com/DevTechnology/DRIC/master/Docker/$dockerfile
+
+#Download war from GIT if PROD
+if [ "$dockerfile" == "dockerfile.prod" ]
+  		then
+    		curl -LO https://github.com/DevTechnology/DRIC/releases/download/$version/$file
+    		if [ ! -f "$file" ]
+    			then
+    				echo "$file not found"
+    				exit
+    			else
+    				chmod 755 $file
+    				mv $file dric-api-webapp.war
+    		fi
+fi
+
 
 #Execute the build
 docker build -f $dockerfile --tag=dtg/dric .
