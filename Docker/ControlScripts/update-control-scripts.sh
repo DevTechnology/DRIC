bbe5d271d5010176c8cb75dfa6b847a1a48d8c13
Lawrence Cutlip-Mason <larry.cutlip-mason@devtechnology.com>
2015-07-07 09:45:37 -0400

    update for branching

--- Docker/ControlScripts/update-control-scripts.sh
+++ Docker/ControlScripts/update-control-scripts.sh
@@ -1,9 +1,9 @@
  #!/bin/bash
  
 #Download from GIT
-curl -O  https://raw.githubusercontent.com/DevTechnology/DRIC/master/Docker/ControlScripts/build-dric-container.sh
-curl -O  https://raw.githubusercontent.com/DevTechnology/DRIC/master/Docker/ControlScripts/start-jboss.sh
-curl -O  https://raw.githubusercontent.com/DevTechnology/DRIC/master/Docker/ControlScripts/stop-jboss.sh
+curl -O  https://raw.githubusercontent.com/DevTechnology/DRIC/DRIC-v1.1-Beta-Release/Docker/ControlScripts/build-dric-container.sh
+curl -O  https://raw.githubusercontent.com/DevTechnology/DRIC/DRIC-v1.1-Beta-Release/Docker/ControlScripts/start-jboss.sh
+curl -O  https://raw.githubusercontent.com/DevTechnology/DRIC/DRIC-v1.1-Beta-Release/Docker/ControlScripts/stop-jboss.sh
 
 #Make the scripts excutable
 chmod 755 *.sh
